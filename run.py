75b6fb4bc1c87b1b08a50601a8b08dd66f817205
Marcel Bednarz <marcel.bednarz16@gmail.com>
2021-06-13 23:26:24 +0200

    work on lab05

--- run.py
+++ run.py
@@ -1,13 +1,37 @@
 from flask import Flask
-from flask import render_template, request, redirect
+from flask import redirect, url_for, render_template, request
+from flask_dance.contrib.github import make_github_blueprint, github
+# import flask_login
+import secrets
+import os
 from AzureDB import AzureDB
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(16)
+# login_manager = flask_login.LoginManager()
+# login_manager.init_app(app)
+
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+
+github_blueprint = make_github_blueprint(
+    client_id="c87d7b00bcb71d06f05c",
+    client_secret="2ad0473c07cce169b011981934e961cdcb23e12a",
+)
+app.register_blueprint(github_blueprint, url_prefix='/login')
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+
+    if not github.authorized:
+        return redirect(url_for('github.login'))
+    else:
+        account_info = github.get('/user')
+        if account_info.ok:
+            account_info_json = account_info.json()
+            return render_template("index.html", data=account_info_json['login'])
+    return '<h1>Request failed!</h1>'
 
 
 @app.route("/aboutme")
@@ -58,6 +82,11 @@ def not_found_error(error):
     return render_template('404.html'), 404
 
 
-if __name__ == '__main__':
+# @app.route("/logout")
+# def logout():
+#     flask_login.logout_user()
+#     return '<h1>You are not authorized!</h1>'
 
+
+if __name__ == '__main__':
     app.run(debug=True)
